681d13a83d424aa5878048c367cf0282c4b3833d
Julien <j.desurmont@lci-group.fr>
2020-05-08 11:41:33 +0200

    change token.txt to login.log

--- src/Ipc/UserBundle/Services/detectLogin.php
+++ src/Ipc/UserBundle/Services/detectLogin.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 class detectLogin {
     // Méthode pour modification de la réponse
     public function enregistreUtilisateur($typeConnexion, $dateConnexion, $token) {
-		$urlFichierToken = getenv("DOCUMENT_ROOT").'/logs/tokenIpcWeb.txt';
+		$urlFichierToken = getenv("DOCUMENT_ROOT").'/logs/login.log';
         $fichierToken = fopen($urlFichierToken, 'a+');
 		// Enregistremùent du token dans le fichier
 		fputs($fichierToken, "$dateConnexion;Connexion $typeConnexion;$token\n");
